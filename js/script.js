ff1de7c2316482353abe29378181a511d1701ea6
Andrii Dovbenko <andrijdovbenko@gmail.com>
2017-01-28 16:21:24 +0200

    new cooments

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $(".review p:last-child").click(function(event){
-    	if(!$(event.delegateTarget.prev()).hasClass('read_more')){
-    		$(event.delegateTarget.prev()).addClass('read_more');
+    	if(!$(event.delegateTarget).prev().hasClass('read_more')){
+    		$(event.delegateTarget).prev().addClass('read_more');
     		$(event.delegateTarget).html('Read More  <i class="fa fa-caret-right" aria-hidden="true">')
     	}
     	else{
-    		$(event.delegateTarget.prev()).removeClass('read_more');
+    		$(event.delegateTarget).prev().removeClass('read_more');
     		$(event.delegateTarget).html('Read Less  <i class="fa fa-caret-up" aria-hidden="true"></i>')
     	}
         
