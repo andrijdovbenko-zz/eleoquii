0af9d66ecf54fec4e596e38191e9a1fb4a6247d3
Andrii Dovbenko <andrijdovbenko@gmail.com>
2017-01-28 16:15:53 +0200

    next commit2

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".review p:last-child").click(function(event){
+    	if(!$(event.delegateTarget.prev()).hasClass('read_more')){
+    		$(event.delegateTarget.prev()).addClass('read_more');
+    		$(event.delegateTarget).html('Read More  <i class="fa fa-caret-right" aria-hidden="true">')
+    	}
+    	else{
+    		$(event.delegateTarget.prev()).removeClass('read_more');
+    		$(event.delegateTarget).html('Read Less  <i class="fa fa-caret-up" aria-hidden="true"></i>')
+    	}
+        
+    });
+});
